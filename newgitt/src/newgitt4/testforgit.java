e328664f4e689f05ca5b7e8f25e51dc7c4b46edc
Dnyaneshwar <Dnyaneshwar@LAPTOP-TCIN3964>
2022-03-16 10:32:57 +0530

    second commit

---
+++ newgitt/src/newgitt4/testforgit.java
@@ -0,0 +1,12 @@
+package newgitt4;
+
+public class testforgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("for test git");
+	}
+	public void test1(){
+		System.out.println("test case pass");
+	}
+}
