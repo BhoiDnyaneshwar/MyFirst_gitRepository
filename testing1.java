f72dea11b86f86d90e328276313f75cbdd236356
DnyaneshwarBhoi <101584183+DnyaneshwarBhoi@users.noreply.github.com>
2022-03-16 09:46:00 +0530

    Add files via upload

---
+++ testing1.java
@@ -0,0 +1,21 @@
+package repo1;
+
+public class testing1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="prasant";
+String b="dnyanu";
+if(a=="prasant") {
+	System.out.println("go to goa");
+	if(b=="dnyanu") {
+		System.out.println("go to rameshwaram");
+	}
+}
+
+
+
+
+	}
+
+}
